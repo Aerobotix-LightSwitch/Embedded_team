89158bea4c4bf056a57fa0809988f71195a321ff
Kyymo <137647536+MehdiAlani@users.noreply.github.com>
2025-09-08 09:38:12 -0200

    old

--- Prototype/MASTER_NODE/encryptionKeyGen.c
+++ Prototype/MASTER_NODE/encryptionKeyGen.c
@@ -5,18 +5,39 @@
 #define KEY_SIZE_IN_BYTES 16
 #define KEY_NAME "ESP_LIGHTNET_ENCRYPTION_KEY"
 
+
+char random_char(){
+    int offset = 'z' - 'A' + 1;
+    return rand() % offset + 'A';
+}
+
+
 int main(){
 
     srand(time(NULL));
 
-    unsigned char random_byte = 0;
     unsigned char i;
-    printf("\n#define %s  {0x%x",KEY_NAME,random_byte);
+    char generated_key[16];
     
-    for(i = 1;i < KEY_SIZE_IN_BYTES;i++){
-        random_byte = 0;
-        printf(",0x%x",random_byte);
+    
+    for(i = 0;i < KEY_SIZE_IN_BYTES;i++){
+        generated_key[i] = random_char();
+    }
+
+    // Hex Format
+    printf("#define %s {0x%x",KEY_NAME,generated_key[0]);
+    for(i = 1 ; i < KEY_SIZE_IN_BYTES;i++){
+        printf(",0x%x",generated_key[i]);
     }
     printf("}\n");
+
+    //String Format
+
+    printf("#define %s \"",KEY_NAME);
+    for(i = 0 ; i < KEY_SIZE_IN_BYTES;i++){
+        printf("%c",generated_key[i]);
+    }
+    printf("\"\n");
+
     return 0;
 }
